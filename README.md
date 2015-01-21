3a53002d3cf51fc7a318ec3c8be8b006ce09e623
Jasper Lu <repsajrules@gmail.com>
2015-01-20 22:18:19 -0500

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+#Accounts-Facebook-ddp
+
+Modification of meteor core package accounts-facebook so that mobile clients can log in with oauth.
+
+Add with:
+
+    meteor add jasperlu:accounts-facebook-ddp
+
+This packages is needed to implement login with OAuth for mobile ddp clients.
---
+++ package.js
@@ -0,0 +1,24 @@
+Package.describe({
+  name: 'jasperlu:accounts-facebook-ddp',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: "Login service for Facebook accounts",
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/jasper-lu/accounts-facebook-ddp',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@0.9.0');
+  api.use('accounts-base', ['client', 'server']);
+  // Export Accounts (etc) to packages using this one.
+  api.imply('accounts-base', ['client', 'server']);
+  api.use('accounts-oauth', ['client', 'server']);
+  api.use('jasperlu:facebook-ddp@0.0.1', ['client', 'server']);
+
+  api.addFiles('facebook_login_button.css', 'client');
+
+  api.addFiles("facebook.js");
+});
