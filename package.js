5e56d76690aa9aee61bee7dcba6f960d673247e4
Vincent Pun <vincent@oddup.com>
2016-08-29 11:42:33 +0800

    Use Oddup facebook-ddp instead

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'jasperlu:accounts-facebook-ddp',
+  name: 'oddup:accounts-facebook-ddp',
   version: '0.0.1',
   // Brief, one-line summary of the package.
   summary: "Login service for Facebook accounts",
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/jasper-lu/accounts-facebook-ddp',
+  git: 'https://github.com/Oddup/accounts-facebook-ddp',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -16,7 +16,7 @@ Package.onUse(function(api) {
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('jasperlu:facebook-ddp@0.0.1', ['client', 'server']);
+  api.use('oddup:facebook-ddp@0.0.1', ['client', 'server']);
 
   api.addFiles('facebook_login_button.css', 'client');
 
